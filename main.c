420bb9b551f9b505dd0ad4d2e25bf42407dc1c92
alijr2018 <alijr_2018@proton.me>
2023-06-23 00:59:39 +0100

    done

--- main.c
+++ main.c
@@ -9,11 +9,10 @@
 
 int main(int argc, char *argv[])
 {
-	stack_t *head = NULL;
-	char  *str = NULL, *operator_array[2], *t;
-	size_t bufsize = 1024, line_count = 0;
+	stack_t *n = NULL;
+	char  *s = NULL, *operator_array[2], *t;
+	size_t bufsize = 1024, j = 0;
 	ssize_t get_line;
-	void (*operator_function)(stack_t **stack, unsigned int L_nbr);
 
 	if (argc != 2)
 		fprintf(stderr, "USAGE: monty file\n"), exit(EXIT_FAILURE);
@@ -22,31 +21,30 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]), exit(EXIT_FAILURE);
 	while (1)
 	{
-		get_line = getline(&str, &bufsize, file);
+		get_line = getline(&s, &bufsize, file);
 		if (get_line == -1)
 			break;
-		line_count++;
-		operator_array[0] = strtok(str, "\n ");
+		j++;
+		operator_array[0] = strtok(s, "\n ");
 		if (operator_array[0] == NULL)
-			_where(&head, line_count);
+			_where(&n, j);
 		else if (strcmp("push", operator_array[0]) == 0)
 		{
 			t = strtok(NULL, "\n ");
-
-			_push(&head, line_count, t);
+			_push(&n, j, t);
 		}
 		else if (operator_array[0] != NULL && operator_array[0][0] != '#')
 		{
-			operator_function = se(operator_array[0], line_count, &head);
+			operator_function = se(operator_array[0], j, &n);
 
-			if (operator_function == NULL && line_count == 0)
+			if (operator_function == NULL && j == 0)
 			{
 				fprintf(stderr, "L%ld: unknown instruction %s\n",
-					line_count, operator_array[0]), exit(EXIT_FAILURE);
+					j, operator_array[0]), exit(EXIT_FAILURE);
 			}
-		operator_function(&head, line_count);
+		operator_function(&n, j);
 		}
 	}
-	fclose(file), free(str), _free(head);
+	fclose(file), free(s), _free(n);
 	return (0);
 }
