b168d405be887a86ae603b83e998114ce8df3bd8
alijr2018 <alijr_2018@proton.me>
2023-06-22 18:34:38 +0100

    done

--- get_add.c
+++ get_add.c
@@ -30,3 +30,85 @@ void get_add(stack_t **stack, unsigned int line_number)
 		free(first);
 	}
 }
+
+/**
+ *  get_free - Free stack
+ *
+ *  @stack: Stack pointer
+ *
+ *  Return: Void
+ */
+
+
+void get_free(stack_t *stack)
+{
+	if (stack)
+	{
+		get_free(stack->next);
+		free(stack);
+	}
+}
+
+/**
+ *  get_nop - When no operation present
+ *
+ *  @stack: Header of stack
+ *
+ *  @line_number: Integer of line numbers
+ *
+ *  Return: Void
+ */
+
+
+void get_nop(stack_t **stack, unsigned int line_number)
+{
+
+	(void) stack;
+	(void) line_number;
+}
+
+/**
+ * get_pall - function that prints all elements on the stack
+ * @stack: pointer to head of the stack
+ * @line_number: where the instruction appears
+ * Description: 0. push, pall
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current;
+
+	(void)line_number;
+
+	current = *stack;
+
+	while (current != NULL)
+	{
+		printf("%d", current->n);
+		current = current->next;
+		printf("\n");
+	}
+}
+
+/**
+ * get_pint - function that prints the top element on the stack
+ * @stack: pointer to head of the stack
+ * @line_number: where the instruction appears
+ * Description: 1. pint
+ * Return: see below
+ * 1. upon success, nothing
+ * 2. upon fail, EXIT_FAILURE
+ */
+void get_pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		fclose(file);
+		get_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
